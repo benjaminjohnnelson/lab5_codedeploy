c446117b4b58a69dd12e038f57ad7630375e4fbe
Ben Nelson <bennelson@Bens-MacBook-Pro-3.local>
2018-10-08 22:29:24 -0600

    new updates

---
+++ home/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello, world! You’re at the home index")
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
---
+++ myprojectenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/bennelson/Documents/MISM/2nd_Year_MISM/AWS/developmentEnv/lab5_codedeploy/myprojectenv/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
